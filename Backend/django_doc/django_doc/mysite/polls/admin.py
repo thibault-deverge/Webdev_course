01dcddccdd39c55ada32b79298f0fcd5aab7270d
thibault <thibault.deverge@gmail.com>
2024-03-20 09:18:38 +0100

    begin django rest tuto

--- Backend/django_doc/django_doc/mysite/polls/admin.py
+++ Backend/django_doc/django_doc/mysite/polls/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     fieldset = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
-    inline = [ChoiceInline]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
